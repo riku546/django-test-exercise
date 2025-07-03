264496f5af92fb7eaedbc26b98837af2c899e6ac
nogimura riku <s1f102400460@iniad.org>
2025-07-03 14:17:36 +0900

    testをしゅうせい

--- todo/tests.py
+++ todo/tests.py
@@ -123,8 +123,8 @@ class TodoViewTestCase(TestCase):
         response = client.get('/{}/'.format(task.pk))
         
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.templates[0], 'todo/detail.html')
-        self.assertEqual(response.content['task'], task)
+        self.assertEqual(response.templates[0].name, 'todo/detail.html')
+        self.assertEqual(response.context['task'], task)
         
     def test_detail_get_fail(self):
         client = Client()
